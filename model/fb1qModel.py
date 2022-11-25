177ccf3c94cb9ef1d617258d6957630f8294ad7c
bocheng0813 <59244639+bocheng0813@users.noreply.github.com>
2022-11-25 21:52:56 +0800

    修改 - 三章一Q

--- model/fb1qModel.py
+++ model/fb1qModel.py
@@ -3,16 +3,16 @@ import json
 
 def insert(account, Tracecode, fb1q_type):
     print("this is po")
-    typesql = "select 4b1q_id from `4b1q_type` where 4b1q = ?",(fb1q_type,)
-    print(typesql)
-    type = DB.execution(DB.select, typesql)
-    sqlstr = "select Tracecode from 4b1q where Tracecode =? AND 4b1q_type=?", (Tracecode,type,)
+    sqlstr = "select 4b1q_id from `4b1q_type` where 4b1q = \"%s\"" % (fb1q_type)
+    print(sqlstr)
+    data = DB.execution(DB.select, sqlstr)
+    sqlstr = "select Tracecode from 4b1q where Tracecode =\"%s\" AND 4b1q_type= \"%s\"" % (Tracecode,data)
     print(sqlstr)
     data = DB.execution(DB.select, sqlstr)
     if data is None:
         return "不存在"
     else:
-        sqlstr = "select id from 4b1q where Tracecode =? AND 4b1q_type=?", (Tracecode,type,)
+        sqlstr = "select id from 4b1q where Tracecode = \"%s\" AND 4b1q_type= \"%s\"" % (Tracecode,type)
         print(sqlstr)
         id = DB.execution(DB.select, sqlstr)
         sqlstr = "insert into 4b1q_user (account,id) VALUES (\"%s\",\"%s\")" % (account, id)
